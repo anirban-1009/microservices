3ca8305fd5bc7841ff2d9a218583176890cc5688
anirban-seanergy <anirban.s@seanergydigital.com>
2024-02-08 12:53:18 +0530

    checking something

--- User/models.py
+++ User/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=140, default='password@123')
     preferred_language = models.CharField(max_length=20, blank=True, choices=[("en", "English"), ("fr", "French"), ("es", "Spanish")], default="en")
     receive_notifications = models.BooleanField(default=True)
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["phone"]
     USERNAME_FIELD = "email"
     objects = UserManager()
     
