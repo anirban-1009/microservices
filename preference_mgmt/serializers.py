cca85ae14c41348451852cbaf9c1e23cfe86f526
anirban-seanergy <anirban.s@seanergydigital.com>
2024-02-08 15:42:23 +0530

    prefences updated

--- preference_mgmt/serializers.py
+++ preference_mgmt/serializers.py
@@ -4,4 +4,9 @@ from User.models import User
 class UserPreferenceUpdateSerialzier(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['phone', 'first_name', 'last_name']
+        fields = ['phone', 'first_name', 'last_name', 'gender']
+
+class UserCommPreferenceUpdateSerialzier(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['preferred_language', 'receive_notifications']
--- preference_mgmt/urls.py
+++ preference_mgmt/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from preference_mgmt.views import UpdateUserViewSet
+from preference_mgmt.views import UpdateUserViewSet, UpdateCommViewSet
 
 router = DefaultRouter()
 router.register('user-detail', UpdateUserViewSet, basename='user-detail')
+router.register('user-communication', UpdateCommViewSet, basename='user-communication')
 
 urlpatterns = [
     path('', include(router.urls)),
