252dce896413fe0637ae6ca09749e19f363aa39b
anirban-seanergy <anirban.s@seanergydigital.com>
2024-02-08 14:54:04 +0530

    login and logout added

--- user_mgmt/urls.py
+++ user_mgmt/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from user_mgmt.views import UsersDetailViewSet, UsersListViewSet
+from user_mgmt.views import UsersDetailViewSet, UsersListViewSet, LoginAPIView, LogOutAPIView
 
 router = DefaultRouter()
 router.register('', UsersListViewSet, basename='user-list')
 router.register(r'detail', UsersDetailViewSet, basename='user-detail')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('auth', include(router.urls)),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogOutAPIView.as_view())
 ]
