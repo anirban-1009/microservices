238f783a2c769c847f475675cd659c20993d3a58
anirban-seanergy <anirban.s@seanergydigital.com>
2024-02-08 11:45:15 +0530

    Added pwd check and detail and delete view for tokens

--- tokenizer/urls.py
+++ tokenizer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from tokenizer.views import TokensListViewSet
+from tokenizer.views import TokensListViewSet, TokensDetailViewSet
 
 router = DefaultRouter()
-router.register(r'list', TokensListViewSet, basename='token-list')
+router.register('list', TokensListViewSet, basename='token-list')
+router.register(r'detail', TokensDetailViewSet, basename='token-detail')
 
 urlpatterns = [
     path('', include(router.urls)),
--- tokenizer/views.py
+++ tokenizer/views.py
@@ -4,6 +4,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from tokenizer.serializer import TokensListSerializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
 
 User = get_user_model()
 
@@ -18,10 +19,32 @@ class TokensListViewSet(viewsets.GenericViewSet):
     def create(self, request):
         user_mail = request.data["username"]
         user = User.objects.get(email=user_mail)
-        token, created = Token.objects.get_or_create(user=user)
-        response = {
-            'token': token.key,
-            'user': user_mail,
-            'created' : created
-        }
-        return  Response(response, status=status.HTTP_200_OK)
+        pwd = request.data["password"]
+        if check_password(pwd, user.password):
+            token, created = Token.objects.get_or_create(user=user)
+            response = {
+                'token': token.key,
+                'user': user_mail,
+                'created' : created,
+                'created_at': token.created
+            }
+            return  Response(response, status=status.HTTP_200_OK)
+        return Response({'msg': 'UnAuthotized'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+class TokensDetailViewSet(viewsets.ModelViewSet):
+    serializer_class = TokensListSerializers
+    queryset = Token.objects.all()
+
+    def retrieve(self, request, pk=None):
+        response = self.serializer_class(self.get_object())
+        return Response(response.data,status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        token = self.get_object()
+        user = request.data["username"]
+        pwd = request.data["password"]
+        if str(user)==str(token.user) and check_password(pwd, ):
+            # token.delete()
+            return Response({'msg': 'deleted'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'msg': 'UnAuthorized'}, status=status.HTTP_401_UNAUTHORIZED)
