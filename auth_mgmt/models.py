e4331972b5b9174ebd876d7765f63443940566c1
anirban-seanergy <anirban.s@seanergydigital.com>
2024-02-09 12:18:08 +0530

    Post model added

--- auth_mgmt/models.py
+++ auth_mgmt/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from User.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ManyToManyField(User, related_name='author', blank=False)
+    body = models.TextField(max_length=500)
